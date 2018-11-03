dac87ec0d315873d5ab0a52d8040bb4b22ce868e
Hamed Momeni <hamed.ma7@gmail.com>
2018-11-03 22:40:18 +0330

    added reverb to karaoke

--- app/src/main/cpp/Karaoke.cpp
+++ app/src/main/cpp/Karaoke.cpp
@@ -11,6 +11,8 @@
 #include <SLES/OpenSLES_AndroidConfiguration.h>
 #include <SLES/OpenSLES.h>
 #include <SuperpoweredRecorder.h>
+#include <SuperpoweredMixer.h>
+#include <SuperpoweredReverb.h>
 
 #define log_write __android_log_write
 #define log_print __android_log_print
@@ -19,11 +21,20 @@ const char *KTAG = "KaraokeCPP";
 
 static SuperpoweredAndroidAudioIO *audioIO;
 static SuperpoweredAdvancedAudioPlayer *player;
-static SuperpoweredRecorder *recorder;
-static float *floatBuffer;
+static SuperpoweredStereoMixer *mixer;
+static SuperpoweredReverb *reverb;
+
+static float *micBuffer;
+static float *reverbBuffer;
+static float *playerBuffer;
+static float *outputBuffer;
+static float *inputs[4];
+static float *outputs[2];
+
+static float inputLevels[8] = {1, 1, 1, 1, 0, 0, 0, 0};
+static float outputLevels[2] = {1, 1};
+
 static float volume = 1;
-bool isRecording = false;
-const char *finalPath = "";
 
 static bool audioProcessing(
         void *__unused clientdata, // custom pointer
@@ -31,15 +42,58 @@ static bool audioProcessing(
         int numberOfFrames,         // number of frames to process
         int __unused samplerate     // sampling rate
 ) {
-    if (player->process(floatBuffer, false, (unsigned int) numberOfFrames, volume)) {
-        SuperpoweredFloatToShortInt(floatBuffer, audio, (unsigned int) numberOfFrames);
-        if (isRecording) {
-            recorder->process(floatBuffer, (unsigned int) numberOfFrames);
+    SuperpoweredShortIntToFloat(audio, micBuffer, (unsigned int) numberOfFrames);
+    SuperpoweredVolume(micBuffer, micBuffer, 3, 3, (unsigned int) numberOfFrames);
+
+    if (reverb->enabled && player->playing) {
+        bool haveMusic = player->process(playerBuffer, false, (unsigned int) numberOfFrames,
+                                         volume);
+        bool haveReverb = reverb->process(micBuffer, reverbBuffer, (unsigned int) numberOfFrames);
+        outputs[0] = outputBuffer;
+        outputs[1] = NULL;
+        inputs[2] = inputs[3] = NULL;
+        if (haveMusic) {
+            inputs[0] = playerBuffer;
+        } else {
+            inputs[0] = NULL;
         }
+        if (haveReverb) {
+            inputs[1] = reverbBuffer;
+        } else {
+            inputs[1] = NULL;
+        }
+        mixer->process(inputs, outputs, inputLevels, outputLevels, NULL, NULL,
+                       (unsigned int) numberOfFrames);
+        SuperpoweredFloatToShortInt(outputBuffer, audio, (unsigned int) numberOfFrames);
+        if (haveMusic || haveReverb) {
+            return true;
+        }
+    } else if (reverb->enabled && !player->playing) {
+        if (reverb->process(micBuffer, reverbBuffer, (unsigned int) numberOfFrames)) {
+            SuperpoweredFloatToShortInt(reverbBuffer, audio, (unsigned int) numberOfFrames);
+            return true;
+        }
+    } else if (!reverb->enabled && player->playing) {
+        bool haveMusic = player->process(playerBuffer, false, (unsigned int) numberOfFrames,
+                                         volume);
+        outputs[0] = outputBuffer;
+        outputs[1] = NULL;
+        inputs[2] = inputs[3] = NULL;
+        if (haveMusic) {
+            inputs[0] = playerBuffer;
+        } else {
+            inputs[0] = NULL;
+        }
+        inputs[1] = micBuffer;
+        mixer->process(inputs, outputs, inputLevels, outputLevels, NULL, NULL,
+                       (unsigned int) numberOfFrames);
+        SuperpoweredFloatToShortInt(outputBuffer, audio, (unsigned int) numberOfFrames);
         return true;
     } else {
-        return false;
+        SuperpoweredFloatToShortInt(micBuffer, audio, (unsigned int) numberOfFrames);
+        return true;
     }
+    return false;
 }
 
 
@@ -62,11 +116,6 @@ static void playerEventCallback(
     };
 }
 
-// This is called after the recorder closed the WAV file.
-static void recorderStopped(void *__unused clientdata) {
-    log_write(ANDROID_LOG_DEBUG, "RecorderExample", "Finished recording.");
-    delete recorder;
-}
 
 // StartAudio - Start audio engine and initialize player.
 extern "C" JNIEXPORT void
@@ -74,12 +123,15 @@ Java_com_hmomeni_canto_activities_KaraokeActivity_StartAudio(
         JNIEnv *__unused env,
         jobject  __unused obj,
         jint samplerate,
-        jint buffersize,
-        jstring tempFilePath,
-        jstring finalFilePath
+        jint buffersize
 ) {
     // Allocate audio buffer.
-    floatBuffer = (float *) malloc(sizeof(float) * 2 * buffersize);
+    playerBuffer = (float *) malloc(sizeof(float) * 2 * buffersize);
+    micBuffer = (float *) malloc(sizeof(float) * 2 * buffersize);
+    reverbBuffer = (float *) malloc(sizeof(float) * 2 * buffersize);
+    outputBuffer = (float *) malloc(sizeof(float) * 2 * buffersize);
+
+    mixer = new SuperpoweredStereoMixer();
 
     // Initialize player and pass callback function.
     player = new SuperpoweredAdvancedAudioPlayer(
@@ -89,32 +141,18 @@ Java_com_hmomeni_canto_activities_KaraokeActivity_StartAudio(
             0                               // cachedPointCount
     );
 
-    log_print(ANDROID_LOG_DEBUG, KTAG, "sampleRate=%d, bufferSize=%d", samplerate, buffersize);
-    log_print(ANDROID_LOG_DEBUG, KTAG, "audioProcessing=%p", audioProcessing);
+    reverb = new SuperpoweredReverb((unsigned int) samplerate);
 
     audioIO = new SuperpoweredAndroidAudioIO(
             samplerate,                     // sampling rate
             buffersize,                     // buffer size
-            false,                          // enableInput
+            true,                           // enableInput
             true,                           // enableOutput
             audioProcessing,                // process callback function
             NULL,                           // clientData
             -1,                             // inputStreamType (-1 = default)
             SL_ANDROID_STREAM_MEDIA,        // outputStreamType (-1 = default)
             buffersize * 2                  // latencySamples
-    );
-
-    const char *tFilePath = env->GetStringUTFChars(tempFilePath, 0);
-    finalPath = env->GetStringUTFChars(finalFilePath, 0);
-
-    recorder = new SuperpoweredRecorder(
-            tFilePath,               // The full filesystem path of a temporarily file.
-            (unsigned int) samplerate,   // Sampling rate.
-            1,                  // The minimum length of a recording (in seconds).
-            2,                  // The number of channels.
-            false,              // applyFade (fade in/out at the beginning / end of the recording)
-            recorderStopped,    // Called when the recorder finishes writing after stop().
-            NULL                // A custom pointer your callback receives (clientData).
     );
 }
 
@@ -140,25 +178,6 @@ Java_com_hmomeni_canto_activities_KaraokeActivity_TogglePlayback(
 ) {
     player->togglePlayback();
     SuperpoweredCPU::setSustainedPerformanceMode(player->playing);  // prevent dropouts
-}
-
-// TogglePlayback - Toggle Play/Pause state of the player.
-extern "C" JNIEXPORT void
-Java_com_hmomeni_canto_activities_KaraokeActivity_StartRecording(
-        JNIEnv *__unused env,
-        jobject __unused obj
-) {
-    isRecording = true;
-    recorder->start(finalPath);
-}
-
-// TogglePlayback - Toggle Play/Pause state of the player.
-extern "C" JNIEXPORT void
-Java_com_hmomeni_canto_activities_KaraokeActivity_StopRecording(
-        JNIEnv *__unused env,
-        jobject __unused obj) {
-    isRecording = false;
-    recorder->stop();
 }
 
 // onBackground - Put audio processing to sleep.
@@ -187,7 +206,7 @@ Java_com_hmomeni_canto_activities_KaraokeActivity_Cleanup(
 ) {
     delete audioIO;
     delete player;
-    free(floatBuffer);
+    free(playerBuffer);
 }
 
 // Cleanup - Free resources.
@@ -222,25 +241,29 @@ Java_com_hmomeni_canto_activities_KaraokeActivity_SetVolume(
 }
 
 // Cleanup - Free resources.
+extern "C" JNIEXPORT void
+Java_com_hmomeni_canto_activities_KaraokeActivity_SetReverb(
+        JNIEnv *__unused env,
+        jobject __unused obj,
+        jfloat reverbAmount
+) {
+    if (reverbAmount == 0) {
+        reverb->enable(false);
+    } else {
+        if (!reverb->enabled) {
+            reverb->enable(true);
+        }
+        reverb->setMix(reverbAmount);
+    }
+}
+
+// Cleanup - Free resources.
 extern "C" JNIEXPORT jboolean
 Java_com_hmomeni_canto_activities_KaraokeActivity_IsPlaying(
         JNIEnv *__unused env,
         jobject __unused obj
 ) {
     if (player->playing) {
-        return 1;
-    } else {
-        return 0;
-    }
-}
-
-// Cleanup - Free resources.
-extern "C" JNIEXPORT jboolean
-Java_com_hmomeni_canto_activities_KaraokeActivity_IsRecording(
-        JNIEnv *__unused env,
-        jobject __unused obj
-) {
-    if (isRecording) {
         return 1;
     } else {
         return 0;
--- app/src/main/java/com/hmomeni/canto/activities/KaraokeActivity.kt
+++ app/src/main/java/com/hmomeni/canto/activities/KaraokeActivity.kt
@@ -3,9 +3,7 @@ package com.hmomeni.canto.activities
 import android.content.Context
 import android.media.AudioManager
 import android.os.Bundle
-import android.os.Environment
 import android.support.v7.app.AppCompatActivity
-import android.widget.Toast
 import com.hmomeni.canto.R
 import com.hmomeni.canto.utils.views.VerticalSlider
 import kotlinx.android.synthetic.main.activity_karaoke.*
@@ -22,12 +20,11 @@ class KaraokeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_karaoke)
 
-        filePath = "/mnt/sdcard/Music/07-Mohsen_Chavoshi_To_Dar_Masafate_Barani.mp3"
+        filePath = "/mnt/sdcard/Music/K_140_SomeoneLikeYou.mp3"
 
         initAudio()
 
         playBtn.setOnClickListener {
-            OpenFile(filePath, 0, File(filePath).length().toInt())
             TogglePlayback()
         }
         seekBar.max = 20
@@ -55,15 +52,11 @@ class KaraokeActivity : AppCompatActivity() {
             }
         }
 
-        recordBtn.setOnClickListener {
-            if (!IsPlaying() && !IsRecording()) {
-                Toast.makeText(this, R.string.you_must_play_the_song_first, Toast.LENGTH_SHORT).show()
-            } else if (IsPlaying() && !IsRecording()) {
-                Toast.makeText(this, R.string.recording_started, Toast.LENGTH_SHORT).show()
-                StartRecording()
-            } else {
-                Toast.makeText(this, R.string.recording_stopped, Toast.LENGTH_SHORT).show()
-                StopRecording()
+        reverbSlider.max = 50
+        reverbSlider.progress = 0
+        reverbSlider.onProgressChangeListener = object : VerticalSlider.OnSliderProgressChangeListener {
+            override fun onChanged(progress: Int, max: Int) {
+                SetReverb(progress / 50f)
             }
         }
     }
@@ -95,14 +88,11 @@ class KaraokeActivity : AppCompatActivity() {
         val sampleRate = Integer.parseInt(samplerateString)
         val bufferSize = Integer.parseInt(buffersizeString)
 
-        val tempFilePath = File(filesDir, "temp.wav").absolutePath
-        val finalFilePath = File(Environment.getExternalStorageDirectory(), "out.wav").absolutePath
-
-        StartAudio(sampleRate, bufferSize, tempFilePath, finalFilePath)
-
+        StartAudio(sampleRate, bufferSize)
+        OpenFile(filePath, 0, File(filePath).length().toInt())
     }
 
-    private external fun StartAudio(samplerate: Int, buffersize: Int, tempFilePath: String, finalFilePath: String)
+    private external fun StartAudio(samplerate: Int, buffersize: Int)
     private external fun OpenFile(path: String, offset: Int, length: Int)
     private external fun TogglePlayback()
     private external fun onForeground()
@@ -111,8 +101,7 @@ class KaraokeActivity : AppCompatActivity() {
     private external fun SetPitch(pitchShift: Int)
     private external fun SetTempo(tempo: Double)
     private external fun SetVolume(vol: Float)
-    private external fun StartRecording()
-    private external fun StopRecording()
+    private external fun SetReverb(amount: Float)
     private external fun IsPlaying(): Boolean
-    private external fun IsRecording(): Boolean
+
 }
