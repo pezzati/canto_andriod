3edb4e60130927c8e2212b9519af05d441d3089a
Hamed Momeni <hamed.ma7@gmail.com>
2018-12-25 10:49:06 +0330

    user edit activity view done

---
+++ app/src/main/java/com/hmomeni/canto/activities/EditUserActivity.kt
@@ -0,0 +1,12 @@
+package com.hmomeni.canto.activities
+
+import android.os.Bundle
+import com.hmomeni.canto.R
+
+class EditUserActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_user)
+    }
+}
